ffd676f0f9243b7130418f3b515b7274f526d789
PC <PC@com>
2019-12-15 19:02:46 +0530

    war configuration

--- src/main/java/com/app/main/Entry.java
+++ src/main/java/com/app/main/Entry.java
@@ -3,6 +3,8 @@ package com.app.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,10 +14,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories("com.app.repository")
 @EntityScan("com.app.model")
 @EnableScheduling
-public class Entry {
-	
+//for war
+public class Entry extends SpringBootServletInitializer {
+
+//for JAR
+//public class Entry{
 	public static void main(String[] args) {
 		SpringApplication.run(Entry.class, args);
+	}
+
+//for war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Entry.class);
 	}
 
 }
